36e95b5b9859ca5ad4a80c825fece727f4cdcb18
Saidakbar_Pardaboyev <pardaboyevsaidakbar103@gmail.com>
2024-09-26 10:11:33 +0500

    made format correct

--- function.go
+++ function.go
@@ -1,51 +1,35 @@
 package function
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"net/url"
 )
 
 type Config struct {
-	BotToken   string
-	AccountIds []string
+	BotToken     string
+	AccountIds   []string
+	FunctionName string
 }
 
 type TelegramBot struct {
 	Cfg Config
 }
 
-func (t *TelegramBot) SendTelegram(text string) error {
+func (o *TelegramBot) SendTelegram(text string) error {
 	client := &http.Client{}
 
-	for _, e := range t.Cfg.AccountIds {
-		// URL encode the text
-		encodedText := url.QueryEscape(text)
-
-		// Include parse_mode=Markdown for formatting
-		botUrl := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s&parse_mode=Markdown", t.Cfg.BotToken, e, encodedText)
+	for _, e := range o.Cfg.AccountIds {
+		botUrl := fmt.Sprintf("https://api.telegram.org/bot"+o.Cfg.BotToken+"/sendMessage?chat_id="+e+"&text=%s", text)
 		request, err := http.NewRequest("GET", botUrl, nil)
 		if err != nil {
-			return fmt.Errorf("failed to create request: %w", err)
+			return err
 		}
 
 		resp, err := client.Do(request)
 		if err != nil {
-			return fmt.Errorf("failed to send request: %w", err)
+			return err
 		}
-		defer resp.Body.Close() // Use defer here to ensure the body is closed
-
-		// Check the response status
-		if resp.StatusCode != http.StatusOK {
-			var errorResponse struct {
-				Description string `json:"description"`
-			}
-			if err := json.NewDecoder(resp.Body).Decode(&errorResponse); err != nil {
-				return fmt.Errorf("failed to decode error response: %w", err)
-			}
-			return fmt.Errorf("failed to send message: %s", errorResponse.Description)
-		}
+		resp.Body.Close()
 	}
 
 	return nil
