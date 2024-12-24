63f9022a43d6c6bf57c9ca08ef8cb7fa0feb6540
Saidakbar <saidakbarpardaboyev@udevs.io>
2024-12-24 10:43:57 +0500

    added function name and error and info signitures to sending telegram text

--- function.go
+++ function.go
@@ -3,20 +3,39 @@ package function
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
-type Config struct {
-	BotToken     string
-	AccountIds   []string
-	FunctionName string
-}
+type (
+	Config struct {
+		BotToken     string
+		AccountIds   []string
+		FunctionName string
+	}
 
-type TelegramBot struct {
-	Cfg Config
-}
+	TelegramBot struct {
+		Cfg Config
+	}
+)
 
-func (o *TelegramBot) SendTelegram(text string) error {
+var (
+	InfoSigniture  = " --- 🔵INFO --- "
+	ErrorSigniture = " -- 🔴ERROR -- "
+)
+
+func (o *TelegramBot) SendTelegram(text string, status ...string) error {
 	client := &http.Client{}
+
+	var (
+		message = o.Cfg.FunctionName
+	)
+
+	if len(status) > 0 && status[0] == "error" {
+		message += " " + ErrorSigniture
+	} else {
+		message += " " + InfoSigniture
+	}
+	text = message + time.Now().Format("2006-01-02 15:04:05") + " " + text
 
 	for _, e := range o.Cfg.AccountIds {
 		botUrl := fmt.Sprintf("https://api.telegram.org/bot"+o.Cfg.BotToken+"/sendMessage?chat_id="+e+"&text=%s", text)
@@ -34,3 +53,19 @@ func (o *TelegramBot) SendTelegram(text string) error {
 
 	return nil
 }
+
+// var (
+// 	botToken = "7508206728:AAEDli0feSjSVqhbLs3CR4GCZssbbPy9LBY"
+// 	chatId   = []string{"2066773119"}
+// )
+
+// func main() {
+
+// 	var bot = TelegramBot{Cfg: Config{
+// 		BotToken:     botToken,
+// 		AccountIds:   chatId,
+// 		FunctionName: "brrauf-function",
+// 	}}
+
+// 	bot.SendTelegram("hello", "error")
+// }
